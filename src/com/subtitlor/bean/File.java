66feb2bd7db12813828e89a9c1709dcdf8c07baf
JEROME0905 <jerome.lebail@dbmail.com>
2019-04-06 12:51:47 +0200

    Création du bean file

---
+++ src/com/subtitlor/bean/File.java
@@ -0,0 +1,14 @@
+package com.subtitlor.bean;
+
+public class File {
+
+    private String nameFile;
+
+    public String getNameFile() {
+        return nameFile;
+    }
+
+    public void setNameFile(String nameFile) {
+        this.nameFile = nameFile;
+    }
+}
